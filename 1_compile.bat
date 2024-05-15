5f3fce2522b9af868d22c7d503914011aa26b9d0
blairecas <chaminealex@mail.ru>
2024-05-15 11:22:19 +0300

    a bit of cleanup

--- 1_compile.bat
+++ 1_compile.bat
@@ -4,53 +4,34 @@ echo.
 echo ===========================================================================
 echo Graphics
 echo ===========================================================================
-..\..\php5\php.exe -f convert_spr.php ./graphics/Tiles.png
-..\..\php5\php.exe -f convert_spr.php ./graphics/Sprites.png
-..\..\php5\php.exe -f convert_font.php
+php -f convert_spr.php ./graphics/Tiles.png
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f convert_spr.php ./graphics/Sprites.png
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f convert_font.php
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 
 echo.
 echo ===========================================================================
 echo Compiling CPU
 echo ===========================================================================
-..\..\php5\php.exe -f ..\scripts\preprocess.php cpu.mac
+php -f ..\scripts\preprocess.php cpu.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 ..\scripts\macro11.exe -ysl 32 -yus -l _cpu.lst _cpu.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-
-echo.
-echo ===========================================================================
-echo Make packed BIN
-echo ===========================================================================
-..\..\php5\php.exe -f ..\scripts\lst2bin.php _cpu.lst _cpu.bin bin 1000
+php -f ..\scripts\lst2bin.php _cpu.lst _cpu.bin bin 1000
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 ..\scripts\zx0 -f _cpu.bin _cpu_lz.bin
-
-rem echo.
-rem echo ===========================================================================
-rem echo Music and player
-rem echo ===========================================================================
-rem ..\..\php5\php.exe -f ..\scripts\preprocess.php menumusic.mac
-rem if %ERRORLEVEL% NEQ 0 ( exit /b )
-rem rem ..\scripts\macro11.exe -ysl 32 -yus -l _menumusic.lst _menumusic.mac
-rem if %ERRORLEVEL% NEQ 0 ( exit /b )
-rem ..\..\php5\php.exe -f ..\scripts\lst2bin.php _menumusic.lst _menumusic.bin bin 100000
-rem ..\scripts\zx0 -f _menumusic.bin _menumusic_lz.bin
 
 echo.
 echo ===========================================================================
 echo Compiling KRAKOUT
 echo ===========================================================================
-..\..\php5\php.exe -f ..\scripts\preprocess.php krakout.mac
+php -f ..\scripts\preprocess.php krakout.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
 ..\scripts\macro11.exe -ysl 32 -yus -l _krakout.lst _krakout.mac
 if %ERRORLEVEL% NEQ 0 ( exit /b )
-
-echo.
-echo ===========================================================================
-echo Linking KRAKOUT
-echo ===========================================================================
-..\..\php5\php.exe -f ..\scripts\lst2bin.php _krakout.lst ./release/krakout.bin bbk 2000
+php -f ..\scripts\lst2bin.php _krakout.lst ./release/krakout.bin bbk 2000
 
 
 echo.
