00ec5e49fd738529f3fa3735f5f3d5f5d5d5624f
blairecas <chaminealex@mail.ru>
2024-07-13 15:03:18 +0300

    win screen and music

--- 1_compile_krakout.sh
+++ 1_compile_krakout.sh
@@ -1,49 +1,45 @@
-@echo off
+#!/bin/sh
 
-echo.
 echo ===========================================================================
 echo Graphics
 echo ===========================================================================
 php -f convert_spr.php ./graphics/Tiles.png
-if %ERRORLEVEL% NEQ 0 ( exit /b )
+[ $? -eq 0 ] || exit 1
 php -f convert_spr.php ./graphics/Sprites.png
-if %ERRORLEVEL% NEQ 0 ( exit /b )
+[ $? -eq 0 ] || exit 1
 php -f convert_font.php
-if %ERRORLEVEL% NEQ 0 ( exit /b )
+[ $? -eq 0 ] || exit 1
 
-echo.
 echo ===========================================================================
 echo Compiling MUSIC
 echo ===========================================================================
 php -f ../scripts/preprocess.php music.mac
-if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\macro11 -ysl 32 -yus -l _music.lst _music.mac
-if %ERRORLEVEL% NEQ 0 ( exit /b )
+[ $? -eq 0 ] || exit 1
+../scripts/macro11 -ysl 32 -yus -l _music.lst _music.mac
+[ $? -eq 0 ] || exit 1
 php -f ../scripts/lst2bin.php _music.lst _music.bin bin 100000
-if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\zx0 -f _music.bin _music_lz.bin
+[ $? -eq 0 ] || exit 1
+../scripts/zx0 -f _music.bin _music_lz.bin
+[ $? -eq 0 ] || exit 1
 
-echo.
 echo ===========================================================================
 echo Compiling CPU
 echo ===========================================================================
 php -f ../scripts/preprocess.php cpu.mac
-if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\macro11 -ysl 32 -yus -l _cpu.lst _cpu.mac
-if %ERRORLEVEL% NEQ 0 ( exit /b )
+[ $? -eq 0 ] || exit 1
+../scripts/macro11 -ysl 32 -yus -l _cpu.lst _cpu.mac
+[ $? -eq 0 ] || exit 1
 php -f ../scripts/lst2bin.php _cpu.lst _cpu.bin bin 1000
-if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\zx0 -f _cpu.bin _cpu_lz.bin
+[ $? -eq 0 ] || exit 1
+../scripts/zx0 -f _cpu.bin _cpu_lz.bin
+[ $? -eq 0 ] || exit 1
 
-echo.
 echo ===========================================================================
 echo Compiling KRAKOUT
 echo ===========================================================================
 php -f ../scripts/preprocess.php krakout.mac
-if %ERRORLEVEL% NEQ 0 ( exit /b )
-..\scripts\macro11 -ysl 32 -yus -l _krakout.lst _krakout.mac
-if %ERRORLEVEL% NEQ 0 ( exit /b )
+[ $? -eq 0 ] || exit 1
+../scripts/macro11 -ysl 32 -yus -l _krakout.lst _krakout.mac
+[ $? -eq 0 ] || exit 1
 php -f ../scripts/lst2bin.php _krakout.lst ./release/krakout.bin bbk 2000
-
-
-echo.
+[ $? -eq 0 ] || exit 1
--- run_bk2010.bat
+++ run_bk2010.bat
@@ -1,3 +1,3 @@
 copy .\release\krakout.bin ..\..\bk2010\files\
 cd ..\..\bk2010
-@start javaw -jar bk2010-0.7.jar -fullscreen 0 -bk0011m -bin krakout
+@start javaw -jar bk2010-0.8-alpha2.jar -fullscreen 0 -bk0011m -bin krakout
